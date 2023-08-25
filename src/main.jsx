84f897ab5aa08a2beb064f484daf64ed98f404f0
lawrenceuchenye <lawuche29@gmail.com>
2023-08-25 20:41:02 +0100

    learning react awesome

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,11 @@
 import React from "react";
 import ReactDom from "react-dom";
-
-//react conponents nust be uppercase
-/*const Hello = () => {
-  return (
-    <div>
-      <h1>Hello world! Vreactie</h1>
-    </div>
-  );
-};*/
-
-const Hello = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "hello world")
-  );
-};
+import App from "./App";
 
 //react dom
-ReactDom.render(<Hello />, document.getElementById("root"));
+ReactDom.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
