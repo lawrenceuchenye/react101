975dc14a6390f8e91c371fdb65f6bc98b7fb5b99
lawrenceuchenye <lawuche29@gmail.com>
2023-09-02 21:21:04 +0100

    done with basics new branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
+const Parent = ({ children }) => {
+  return (
+    <div
+      style={{ padding: "30px", background: "#cf1733", borderRadius: "8px" }}
+    >
+      {children}
+    </div>
+  );
+};
+
+const Child = ({ id }) => {
+  const alertId = (cid) => {
+    console.log(`Child ${cid}`);
+  };
+
+  return (
+    <div
+      style={{
+        padding: "30px",
+        background: "#0de",
+        borderRadius: "8px",
+        margin: "10px",
+      }}
+      onClick={alertId(id)}
+    ></div>
+  );
+};
+
 const App = () => {
   return (
     <div>
-      <h1>Hello world! Vreactie</h1>
+      <Parent>
+        <Child id={1} />
+        <Child id={2} />
+        <Child id={3} />
+      </Parent>
     </div>
   );
 };
