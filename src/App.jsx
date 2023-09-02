30587fd5779424f71c26be1b93faa120f75a358c
lawrenceuchenye <lawuche29@gmail.com>
2023-09-02 21:33:37 +0100

    new era

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,6 @@
-const Parent = ({ children }) => {
-  return (
-    <div
-      style={{ padding: "30px", background: "#cf1733", borderRadius: "8px" }}
-    >
-      {children}
-    </div>
-  );
-};
-
-const Child = ({ id }) => {
-  const alertId = (cid) => {
-    console.log(`Child ${cid}`);
-  };
-
-  return (
-    <div
-      style={{
-        padding: "30px",
-        background: "#0de",
-        borderRadius: "8px",
-        margin: "10px",
-      }}
-      onClick={alertId(id)}
-    ></div>
-  );
-};
-
 const App = () => {
   return (
     <div>
-      <Parent>
-        <Child id={1} />
-        <Child id={2} />
-        <Child id={3} />
-      </Parent>
     </div>
   );
 };
