467847817d3963e045da57c38631d7f3c61bb3ed
lawrenceuchenye <lawuche29@gmail.com>
2023-09-10 14:48:10 +0100

    possibly merge later

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import "./App.css";
+import { useState } from "react";
 
 const App = () => {
+  let [text, setText] = useState("text");
+  console.log(useState("bread"));
+
+  const changeText = () => {
+    setText("Hello twitter");
+  };
+
   return (
     <div>
-      <h1>Intermidate React</h1>
+      <h1>{text}</h1>
+      <button className="btn" onClick={() => changeText()}>
+        Click me!
+      </button>
     </div>
   );
 };
