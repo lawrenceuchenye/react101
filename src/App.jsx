93affcdd967a79ec68daa9c11b5dd6aaa49cbc91
lawrenceuchenye <lawuche29@gmail.com>
2023-09-21 23:10:23 +0100

    still on

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import "./App.css";
 import { useState, useEffect } from "react";
 
 const App = () => {
+  const [show, setShow] = useState(false);
 
   return (
     <div className="main-container">
-      <h1>Desktop</h1>
+      {show ? <h1>Twitter</h1> : <h1> Secret Text</h1>}
+      <div>
+        <button className="btn" onClick={() => setShow(!show)}>
+          {show ? "Btn Clicked" : "Click Me!"}
+        </button>
+      </div>
     </div>
   );
 };
