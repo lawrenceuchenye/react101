76970a2c5e767f4fa62d3a418ff6654151c7c432
lawrenceuchenye <lawuche29@gmail.com>
2023-09-21 22:46:37 +0100

    cleaned up

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,6 @@ import "./App.css";
 import { useState, useEffect } from "react";
 
 const App = () => {
-  const [count, setCounter] = useState(0);
-  const [isMobile, setIsMobile] = useState(false);
-
-  const addCounter = () => {
-    setCounter(count + 1);
-  };
-
-  useEffect(() => {
-    if (window.innerWidth <= 750) {
-      setIsMobile(true);
-    } else {
-      setIsMobile(false);
-    }
-  }, []);
-
-  if (isMobile) {
-    return (
-      <div className="main-container">
-        <h1>Mobile</h1>
-      </div>
-    );
-  }
 
   return (
     <div className="main-container">
