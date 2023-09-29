016cb4805359a2ee87d92772ae410bc602994664
lawrenceuchenye <lawuche29@gmail.com>
2023-09-29 17:28:30 +0100

    useref here we come

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,46 @@
 import "./App.css";
 import "./Form.css";
 
+import { useState } from "react";
+
 const Form = () => {
+  const [name, setName] = useState("coder");
+  const [password, setPassword] = useState("password");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    alert("hello");
+  };
+
   return (
     <div className="main-form-container">
-      <form className="form-container">
+      <form className="form-container" onSubmit={(e) => onSubmit(e)}>
         <div className="input-container">
           <label>Name:</label>
-          <input type="text" placeholder="Username" />
+          <input
+            type="text"
+            placeholder="Username"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
         <div className="input-container">
           <label>Password:</label>
-          <input type="password" placeholder="Password" />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChnage={(e) => setPassword(e.target.value)}
+          />
         </div>
-        <button>Login</button>
+        <button type="submit" className="btn-container">
+          Login
+        </button>
       </form>
     </div>
   );
 };
+
 const App = () => {
   return (
     <div className="main-container">
