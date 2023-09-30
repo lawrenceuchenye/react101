a725864a68574ddb6e8fc147700b74661ab59b92
lawrenceuchenye <lawuche29@gmail.com>
2023-09-30 22:46:15 +0100

    prop drilling >>>

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,12 @@ import "./App.css";
 
 import { useState, useReducer } from "react";
 
-const defaultState = {
-  NAME: "CODER",
-  PLATFORM: "TWITTER",
-};
-const reducer = (state, action) => {
-  if (action.TYPE == "CHANGE") {
-    //when an actiom is excuted it returns a mutated state.
-    return { ...state, NAME: "LAW", PLATFORM: "X(EVERYTHING APP)" };
-  }
 
-  if (action.TYPE == "NEW NAME") {
-    return { ...state, NAME: action.PAYLOAD };
-  }
-
-  //error exist.
-  throw Error("TYPE EXCUTED DOE NOT EXIST");
-};
 
 const App = () => {
-  const [state, Dispatch] = useReducer(reducer, defaultState);
   return (
     <div className="main-container">
       <h1>useReducers</h1>
-      <div style={{ marginTop: "20px" }}>
-        <h1>NAME:{state.NAME}</h1>
-        <h1>PLATFORM:{state.PLATFORM}</h1>
-        <button
-          className="btn"
-          onClick={() => Dispatch({ TYPE: "NENAME", PAYLOAD: "ELON MUSK" })}
-        >
-          CHANGE
-        </button>
-      </div>
     </div>
   );
 };
