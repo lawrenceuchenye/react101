1ecbf0da895a518a3e3ae52594d7364e5b6ba81e
lawrenceuchenye <lawuche29@gmail.com>
2023-10-01 20:38:51 +0100

    prop drilling started

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,40 @@ import "./App.css";
 
 import { useState, useReducer } from "react";
 
+const Display = ({ val }) => {
+  return (
+    <div className="display">
+      <h1>Messages</h1>
+      <h2>
+        Hello !!! <span>{val}</span>
+      </h2>
+    </div>
+  );
+};
 
+const Buttons = () => {
+  const [val, setValue] = useState(0);
 
+  return (
+    <div>
+      <div>
+        <Display val={val} />
+      </div>
+      <div>
+        <button className="btn" onClick={() => setValue(val + 1)}>
+          +
+        </button>
+        <button className="btn" onClick={() => setValue(val - 1)}>
+          -
+        </button>
+      </div>
+    </div>
+  );
+};
 const App = () => {
   return (
     <div className="main-container">
-      <h1>useReducers</h1>
+      <Buttons />
     </div>
   );
 };
