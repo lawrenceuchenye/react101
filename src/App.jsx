a66dd170dc2b28c32e554e89aa279d5f7f97ee84
lawrenceuchenye <lawuche29@gmail.com>
2023-10-02 22:46:30 +0100

    finally

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import "./App.css";
 import { useFetch } from "./Hooks/";
+import { useState, useEffect } from "react";
 
-const url = "github.com/user/repo/main/db.json";
+const url = "https://dummyjson.com/carts";
 
 const App = () => {
   const { loading, data } = useFetch(url);
+
   console.log(data);
+  console.log("we're here");
 
   return (
     <div className="main-container">
       <div>
-        <h1>{isLoading ? "laoding..." : "loaded"}</h1>
+        <h1>{loading ? "laoding..." : "loaded"}</h1>
       </div>
     </div>
   );
--- src/Hooks/index.jsx
+++ src/Hooks/index.jsx
@@ -2,20 +2,18 @@ import { useState, useEffect } from "react";
 
 export const useFetch = (url) => {
   const [data, setData] = useState(null);
-  const [loading, setIsLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const getData = async () => {
     const data_res = await fetch(url);
     const data_json = await data_res.json();
     setData(data_json);
-    console.log(data_json);
-    console.log("######");
-    setIsLoading(false);
+    setLoading(false);
   };
 
   useEffect(() => {
     getData();
-  }, []);
+  }, [url]);
 
   return { loading, data };
 };
