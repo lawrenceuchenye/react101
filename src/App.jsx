a1e3a3f4c3982a436b25344d457463e879a6acec
lawrenceuchenye <lawuche29@gmail.com>
2023-10-04 22:42:13 +0100

    rrd

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import "./App.css";
-import { useFetch } from "./Hooks/";
-import { useState, useEffect } from "react";
-
-const url = "https://dummyjson.com/carts";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => {
-  const { loading, data } = useFetch(url);
 
-  console.log(data);
-  console.log("we're here");
 
   return (
     <div className="main-container">
       <div>
-        <h1>{loading ? "laoding..." : "loaded"}</h1>
+        <h1>React Router</h1>
       </div>
     </div>
   );
