3bcf5d3f1fb6c266c509d8387d5e2e6f5290f771
lawrenceuchenye <lawuche29@gmail.com>
2023-10-04 23:39:52 +0100

    navbar made

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 const App = () => {
   return (
     <Router>
       <div className="main-container">
+        <div className="nb-container">
+          <span>
+            <Link to="/">Home</Link>
+          </span>
+          <span>
+            <Link to="/contact">Contact</Link>
+          </span>
+          <span>
+            <Link to="/about">About</Link>
+          </span>
+        </div>
         <h1>React Router</h1>
       </div>
     </Router>
